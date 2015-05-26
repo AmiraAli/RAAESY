8bb3e4f788564983ddc8e7d727a6b9a7f7b057f1
AmiraAli <amira_ali_marzouk@outlook.com>
2015-05-26 17:22:32 +0200

    updated database migrations

--- database/migrations/2015_05_26_125253_create_assets_table.php
+++ database/migrations/2015_05_26_125253_create_assets_table.php
@@ -21,6 +21,8 @@ class CreateAssetsTable extends Migration {
 			$table->string('comment');
 			$table->integer('assettype_id')->unsigned();
 			$table->foreign('assettype_id')->references('id')->on('asset_types');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
 			$table->timestamps();
 		});
 	}
