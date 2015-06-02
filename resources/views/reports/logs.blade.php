1edc79ea4bee7eac273020de3e246d556c5bef96
yosra <yosra.m.yasser@gmail.com>
2015-06-03 01:11:07 +0300

    deletion log

--- resources/views/reports/logs.blade.php
+++ resources/views/reports/logs.blade.php
@@ -1,7 +1,14 @@
 <html>
 <head>
 
-     
+<style>
+	span{
+
+		display: block;
+		float:right;
+
+	}
+</style>
 	
 
         
@@ -15,9 +22,43 @@
 <div class="container">
 
 <div  id="con" class="col-md-12" >
-@foreach($logs as $log)
+@foreach ($logs as $log  )
+ <div class="row" >
+ 	<div class="panel panel-default">
 
-{{$log->id}}
+ 	<div class="panel-heading">Done by: <b>{{ ucfirst ($log->user->fname) }}</b> 
+ 	<span> 		
+ 	
+	{{ $log->created_at }} </span></div>
+
+ 	<div class="panel-body">
+ 	<?php $name ='p'; ?>
+
+ 	@if ( $log->type == 'user')
+             <?php $name='name'; ?>                  
+  	@elseif ( $log->type == 'article')
+			<?php $name = 'title'; ?>
+	@else
+			<?php $name = 'subject'; ?>
+	@endif
+
+
+ 	{{$log->user->fname}}
+
+	@if ( $log->action == 'spam')
+		marked the {{$log->type}} #{{$log->id}} with {{$name}} "{{$log->name}}" as spam
+	
+	@else
+		{{$log->action}}d the {{$log->type}} #{{$log->id}} with {{$name}} "{{$log->name}}"
+	@endif
+
+ 	
+	</div>
+
+</div>
+
+	</div>
+	
 @endforeach
 
 
