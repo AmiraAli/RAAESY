c0e76dc4a828148e96afc7a4576c5ea2cd2a4992
AmiraAli <amira_ali_marzouk@outlook.com>
2015-06-09 19:25:04 +0200

    submit design forms

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,10 +8,11 @@
 
 @section('content')
 <div class="container-fluid">
+<br>
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{$user->fname}} {{$user->lname}}</div>
+			<div class="panel ">
+				<div class="panel-heading navbtn txtnav">{{$user->fname}} {{$user->lname}}</div>
 				<div class="panel-body">
 				 @if (count($errors) > 0)
                         <div class="alert alert-danger">
@@ -109,7 +110,7 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
+								<button type="submit" class="btn navbtn txtnav">
 									Done Editing
 								</button>
 							</div>
