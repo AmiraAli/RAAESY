c006b00b9e12f31cfbe09642b4273eda6e927168
samah <sama7.a7med@hotmail.com>
2015-06-09 19:53:37 +0200

    technical statistics

--- resources/views/searchArticle.blade.php
+++ resources/views/searchArticle.blade.php
@@ -1,14 +1,9 @@
 @foreach($articles as $article)
 	<div class="col-md-4 article" >	
 		<div class="panel panel-info">
-<<<<<<< HEAD
 	  		<div class="panel-body"  id="articles">	
-	  			<a href="/articles/{{$article->id}}"><strong>{{$article->subject}}</strong></a><br>		    			
-=======
+	  			<a href="/articles/{{$article->id}}"><strong>{{$article->subject}}</strong></a><br>		    	
 
-	  		<div class="panel-body" >	
-	  			<a  class="navtxt" href="/articles/{{$article->id}}"><strong>{{$article->subject}}</strong></a><br>		    			
->>>>>>> c4ab86c0c8fe713c08d87651d78d753fdd1c8908
 		    	@if (strlen($article->body) <= 100)
 		    		{{ $article->body }}
 		    	@else
