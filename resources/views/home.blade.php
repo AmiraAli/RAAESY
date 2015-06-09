a24b2982368a5acc8a11d3d82079e5ea0c506960
samah <sama7.a7med@hotmail.com>
2015-06-09 19:46:37 +0200

    technical statistics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,12 +23,11 @@
 				</div>
 			</div>
 		</div>
-		<div class="row" id="article-show">
+		<div class="col-md-10" id="article-show">
 			@foreach($articles as $article)
-				<div class="col-md-3 article" >	
+				<div class="col-md-4 article" >	
 					<div class="panel panel-info">
-	
-				  		<div class="panel-body" >	
+				  		<div class="panel-body"  id="articles">	
 				  			<a href="/articles/{{$article->id}}"><strong>{{$article->subject}}</strong></a><br>		    			
 					    	@if (strlen($article->body) <= 100)
 					    		{{ $article->body }}
--- resources/views/searchArticle.blade.php
+++ resources/views/searchArticle.blade.php
@@ -1,8 +1,7 @@
 @foreach($articles as $article)
-	<div class="col-md-3 article" >	
+	<div class="col-md-4 article" >	
 		<div class="panel panel-info">
-
-	  		<div class="panel-body" >	
+	  		<div class="panel-body"  id="articles">	
 	  			<a href="/articles/{{$article->id}}"><strong>{{$article->subject}}</strong></a><br>		    			
 		    	@if (strlen($article->body) <= 100)
 		    		{{ $article->body }}
