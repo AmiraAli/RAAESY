ec12e79f7356a9dc480ab61758e02ff531017811
yosra <yosra.m.yasser@gmail.com>
2015-06-10 15:12:02 +0300

    sender of emails

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,6 +1,7 @@
 
-Hey {{$fname}} {{$lname}}, Welcome to our website. <br>
-Your email is {{$email}}. <br>
-Your phone is {{$phone}}. <br>
-Your location is {{$location}}. <br>
-Your password is {{$password}}. <br>
+Hey {{$fname}} {{$lname}}, Welcome to RSB! <br>
+If you have any problem, please submit a ticket and we will process <br>
+your request ASAP. <br>
+
+Best Wishes, <br>
+RSB Team
