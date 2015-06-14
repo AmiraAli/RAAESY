b6b7a60c595f91bf427e7c91bffabde5530794de
yosra <yosra.m.yasser@gmail.com>
2015-06-14 13:22:52 +0200

    spam user changed

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Auth;
 
 class Authenticate {
 
@@ -41,6 +42,12 @@ class Authenticate {
 			else
 			{
 				return redirect()->guest('auth/login');
+			}
+		}else{
+			if (Auth::User()->isspam == 1){
+				$this->auth->logout();
+				$spamMessage = "You are banned from accessing the server!";
+				return view('auth.login',compact('spamMessage'));
 			}
 		}
 
