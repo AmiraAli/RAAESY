a6e9e2d5501f83abb2f677eb63612b6458ac29b3
samah <sama7.a7med@hotmail.com>
2015-06-17 08:43:00 +0200

    technical statistics

--- resources/views/reports/summary.blade.php
+++ resources/views/reports/summary.blade.php
@@ -16,9 +16,17 @@
 		  		<option value="custom">Custom</option>
 			</select>
 		</div>
-		<div class="col-xs-6 col-sm-5 col-md-5 col-lg-5" style="display:none;" id="customedate">
-			<label class="col-xs-3 col-md-2">From</label><input  class="col-xs-3 col-md-4" type="text" id="startdate">
-			<label class="col-xs-2 col-md-2">To</label><input class="col-xs-3 col-md-4" type="text" id="enddate">
+		<div class="col-xs-6 col-sm-5 col-md-6 col-lg-6" style="display:none;" id="customedate">
+			<form class="form-inline">
+			<div class="form-group">
+				<label>From</label>
+				<input  class="form-control" type="text" id="startdate">
+			</div>
+			<div class="form-group">
+				<label>To</label>
+				<input class="form-control" type="text" id="enddate">
+			</div>
+			</form>
 		</div>
 		<div class=" col-xs-4 col-sm-4 col-md-4 col-lg-4" style="float:left;">
 			<button class="btn navbtn txtnav" onclick="search()"><span class="glyphicon glyphicon-search"></span></button>
