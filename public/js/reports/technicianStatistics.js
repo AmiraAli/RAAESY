2d6c5d131c04d02d0d1d7a4da2335a9dc9bd359c
samah <sama7.a7med@hotmail.com>
2015-06-18 13:19:51 +0200

    finaldes7

--- public/js/reports/technicianStatistics.js
+++ public/js/reports/technicianStatistics.js
@@ -9,22 +9,27 @@ window.onload = function() {
            
 function technicianStatisticsSearch() 
 {
+     if($("#from").val() > $("#to").val()){
 
-    var searchData = {
-            'from'  : $('#from').val() ,
-            'to'    : $('#to').val()
-        };
-		
-    $.ajax({
-	    url: '/reports/technicianStatisticsSearch',
-    	type: 'post',
-    	data: searchData,
-	    success: function(result) {
-			 $('#table_show').html(result);
-			 
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log(errorThrown);
-	    }
-	});	
+        $("#table_show").html("<br><br><div id='error'><center>End date must be greater than start date</center></div>");
+
+    }else{
+        var searchData = {
+                'from'  : $('#from').val() ,
+                'to'    : $('#to').val()
+            };
+    		
+        $.ajax({
+    	    url: '/reports/technicianStatisticsSearch',
+        	type: 'post',
+        	data: searchData,
+    	    success: function(result) {
+    			 $('#table_show').html(result);
+    			 
+    		},
+    		error: function(jqXHR, textStatus, errorThrown) {
+    			console.log(errorThrown);
+    	    }
+    	});	
+    }
 }
